15a69bbbedff0549356eaccf6e878891ebec3395
almusto <ales.musto@gmail.com>
2016-05-17 16:18:10 -0400

    String Assignment Complete

--- BlocExercises/BlocExercises/StringCheese.m
+++ BlocExercises/BlocExercises/StringCheese.m
@@ -11,15 +11,30 @@
 @implementation StringCheese
 
 - (NSString *) favoriteCheeseStringWithCheese:(NSString *)cheeseName {
-    /* WORK HERE */
-    return nil;
+    
+    
+    NSString *favCheese = [NSString stringWithFormat:@"My favorite cheese is %@.", cheeseName];
+    
+    return favCheese;
 }
 
 - (NSString *) cheeseNameWithoutCheeseSuffix:(NSString *)cheeseName {
     if ([[cheeseName lowercaseString] hasSuffix:@" cheese"]) {
         /* WORK HERE, ASSUMING `cheeseName` ENDS WITH " cheese" */
+        
+        
+       return   [cheeseName stringByReplacingCharactersInRange:
+                 [[cheeseName lowercaseString] rangeOfString:@" cheese"]
+                                                                     withString:@""];
+    
+        //ANOTHER ALTENRATIVE BELOW 
+        //return [cheeseName stringByReplacingOccurrencesOfString:@" cheese" withString:@""];
+        
     } else {
         /* WORK HERE, ASSUMING `cheeseName` DOES NOT END WITH " cheese" */
+        
+        return cheeseName;
+        
     }
 
     /*
@@ -32,8 +47,14 @@
 - (NSString *) numberOfCheesesStringWithCheeseCount:(NSUInteger)cheeseCount {
     if (cheeseCount == 1) {
         /* WORK HERE, ASSUMING THERE IS 1 CHEESE */
+        
+        return [NSString stringWithFormat:@"1 cheese"];
+        
+        
     } else {
         /* WORK HERE, ASSUMING THERE ARE 2+ CHEESES */
+        
+        return [NSString stringWithFormat:@"%lu cheeses", cheeseCount];
     }
     
     /*
